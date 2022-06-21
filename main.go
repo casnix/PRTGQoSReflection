97c44283529515e45103066af1376ee384b5ac1a
casnix <rienzom@southwestern.org>
2022-06-21 11:59:30 -0500

    License boilerplate

--- main.go
+++ main.go
@@ -4,8 +4,18 @@
 /* Written by Matt Rienzo for Southwestern Healthcare, Inc.'s IT */
 /* department.                                                   */
 /*---------------------------------------------------------------*/
-/* Copyright (C) Matt Rienzo, 2022                               */
-/* Licensed under                */
+/* Copyright 2022 Matt Rienzo                                    */
+/*                                                               */
+/* Licensed under the Apache License, Version 2.0 (the           */
+/* "License"); you may not use this file except in compliance    */
+/* with the license.  You may obtain a copy of the license at    */
+/*    http://www.apache.org/licenses/LICENSE-2.0                 */
+/* Unless required by applicable law or agreed to in writing,    */
+/* software distributed under the License is distributed on an   */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,  */
+/* either express or implied.  See the License for specific      */
+/* language governing permissions and limitations under the      */
+/* License.                                                      */
 /*****************************************************************/
 
 
