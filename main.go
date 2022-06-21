88783158929c6a3fc0a985470051e8adbdcc22c7
casnix <rienzom@southwestern.org>
2022-06-21 11:47:16 -0500

    Initial commit, started main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+/*****************************************************************/
+/* main.go -- Entry point for Golang PRTGQoSReflection.          */
+/*                                                               */
+/* Written by Matt Rienzo for Southwestern Healthcare, Inc.'s IT */
+/* department.                                                   */
+/*---------------------------------------------------------------*/
+/* Copyright (C) Matt Rienzo, 2022                               */
+/* Licensed under                */
+/*****************************************************************/
+
+
+import (
+	"os"
+	"net"
+
+	"github.com/jessevdk/go-flags"
+)
+
+
