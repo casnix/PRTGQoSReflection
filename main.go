d0dbb054019a6aa9684a456853e63b9a27fcd0df
casnix <casnix@github.io>
2022-07-05 11:47:07 -0500

    Added support for all CLI switches. Tested successfully against PRTG.

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 
 	"github.com/casnix/PRTGQoSReflection/buildinfo"
 	"github.com/jessevdk/go-flags"
@@ -37,28 +38,12 @@ var cli_opts struct {
 	Port 		string 	`short:"p" long:"port" description:"Provide port defined in PRTG"`
 	Configuration 	string 	`short:"c" long:"conf" description:"Path of config file, if not provided default qosreflect.conf will be used"`
 	Host 		string 	`short:"o" long:"host" description:"Provide the IP address if the interface the script should bind to.\nUse ''All'' to bind to all available interfaces (recommended)"`
-	ReplyIP		string 	`short:"r" long:"replyip" description:"NOT SUPPORTED YET Provide the IP address of the PRTG Probe which sends the packets.\nThe reflector will then only reply to this IP"`
+	ReplyIP		string 	`short:"r" long:"replyip" description:"Provide the IP address of the PRTG Probe which sends the packets.\nThe reflector will then only reply to this IP"`
 	ReplyPort 	string 	`short:"t" long:"replyport" description:"Provide the port the packets should be bounced to"`
-	NATMode 	bool 	`short:"n" long:"nat" description:"NOT SUPPORTED YET Option enables the NAT mode so packets are reflected exactly to the port they are received from"`
-	Debug 		bool 	`short:"d" long:"debug" description:"NOT SUPPORTED YET Set to turn on detailed output"`
+	NATMode		bool	`short:"n" long:"nat-mode" description:"Respond directly to client's source port.  For use when behind NAT/PAT/SNAT.  Recommended to use with -r to restrict responses to the NATing gateway.  TAKES PRECEDENCE OVER --replyport"`
+	Debug 		bool 	`short:"d" long:"debug" description:"Set to turn on detailed output"`
 }
 
-
-// IsFlagPassed(string) bool -- Checks if CLI flag was passed
-// Input: - name string		-- Flag name
-// Output: - true		-- Flag found
-// 	   - false		-- Flag not found
-/*func IsFlagPassed(name string) bool {
-	found := false
-	flag.Visit(fun(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-	
-	return found
-}
-*/
 
 // ParseCLI(void) error -- Parses command line arguments
 // Input: none
@@ -106,25 +91,31 @@ func main() {
 
 	ParseCLI()
 	
-	// FIX THIS, NEED TO CHECK GO-FLAGS FOR IF FLAG IS PASSED
 	// Validate configuration options
 	if false {
 		ReadConfig()
 	}
-	/*if !IsFlagPassed("Configuration") { // No configuration file specified
-		log.Printf("%s debug cli_opts.Configuration == %s", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, cli_opts.Configuration))
+	if cli_opts.Configuration == "" { // No configuration file specified
+		if cli_opts.Debug {
+			log.Printf("%s debug cli_opts.Configuration is %s", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "empty"))
+		}
+
 		// Validate that required options are set in this case
-		if !IsFlagPassed("ReplyIP") || !IsFlagPassed("Host") || !IsFlagPassed("Port") { // We must fall back to the configuration file
-			log.Printf("%s debug cli_opts.(ReplyIP, Host, Port) == (%s, %s, %s)", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, cli_opts.ReplyIP), color.Ize(color.Yellow, cli_opts.Host), color.Ize(color.Yellow, cli_opts.Port))
+		if cli_opts.Port == "" { // We must fall back to the configuration file
+			if cli_opts.Debug {
+				log.Printf("%s debug cli_opts.Port is %s", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "empty"))
+			}
 			cli_opts.Configuration = "qosreflect.conf"
 			ReadConfig()
 		}
 	} else {
 		ReadConfig()
-	}*/
+	}
 
 	if cli_opts.Host != "All" {
 		hOST = cli_opts.Host
+	} else { // Make hOST empty to bind to all IP interfaces.
+		hOST = ""
 	}
 
 	if cli_opts.ReplyPort != "" {
@@ -146,24 +137,47 @@ func main() {
 	defer server.Close()
 	
 	log.Printf("%s%s UDP reflection server is running, listening on %s:%s.\n", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "{0}"), hOST, pORT)
-	// Add check for replyIP.
-	rESTRICT = false
+	if rESTRICT {
+		log.Printf("%s%s Access control rule: %s from %s, %s all others.", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "{0}"), color.Ize(color.Green, "ACCEPT"), color.Ize(color.Yellow, cli_opts.ReplyIP), color.Ize(color.Red, "DROP"))
+	}
+	if cli_opts.NATMode {
+		log.Printf("%s%s Running in NAT mode.", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "{0}"))
+	}
+
 	for {
 		udpBuffer := make([]byte, 1024)
-		index, address, err := server.ReadFrom(udpBuffer)
-		if err != nil && !rESTRICT {
+		index, address, _ := server.ReadFrom(udpBuffer)
+		ipAddress := strings.Split(address.String(), ":")
+		if cli_opts.Debug {
+			log.Printf("%s%s debug server.ReadFrom address = %s, ipAddress = %s", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "{1}"), color.Ize(color.Yellow, address.String()), color.Ize(color.Yellow, ipAddress[0]))
+			log.Printf("%s%s debug server.ReadFrom byte buffer = %X", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "{1}"), udpBuffer)
+			log.Printf("%s%s debug server.ReadFrom byte ASCII = %s", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "{1}"), color.Ize(color.Yellow, string(udpBuffer[:index])))
+		}
+
+		if rESTRICT && ipAddress[0] != cli_opts.ReplyIP {
+			log.Printf("%s%s server.ReadFrom ipAddress does not match cli_opts.ReplyIP! %s", color.Ize(color.Cyan, buildinfo.LogName), color.Ize(color.Yellow, "{1}"), color.Ize(color.Red, "Dropping packet."))
 			continue
 		}
-		go Reflect(server, address, udpBuffer[:index])
+
+		if !cli_opts.NATMode {
+			if cli_opts.ReplyPort == "" {
+				go Reflect(ipAddress[0]+":"+pORT, udpBuffer[:index])
+			} else {
+				go Reflect(ipAddress[0]+":"+cli_opts.ReplyPort, udpBuffer[:index])
+			}
+		} else {
+			go Reflect(address.String(), udpBuffer[:index])
+		}
 	}
 }
 
 
-// Reflect(net.PacketConn, net.Addr, []byte) void -- Reflects/serves UPD packet
-// Input: - net.PacketConn		- PacketConn object with connect data
-//	  - net.Addr			- Addr object with network address data
+// Reflect(string, []byte) void -- Reflects/serves UPD packet
+// Input: - string			- ipAddress:PORT
 //	  - []byte			- Byte buffer with UDP data to serve
 // Outpu: None.
-func Reflect(server net.PacketConn, address net.Addr, udpBuffer []byte) {
-	server.WriteTo(udpBuffer, address)
+func Reflect(client string, udpBuffer []byte) {
+	clientStream, _ := net.Dial("udp", client)
+	clientStream.Write(udpBuffer)
+	defer clientStream.Close()
 }
